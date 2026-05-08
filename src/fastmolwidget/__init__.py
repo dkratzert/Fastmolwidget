6263e76b7cff63f38d48d03ed0acc62a919f13e6
Daniel Kratzert <dkratzert@gmx.de>
2026-05-08 07:23:59 +0200

    bump version to 0.9.4

--- src/fastmolwidget/__init__.py
+++ src/fastmolwidget/__init__.py
@@ -6,7 +6,7 @@ from fastmolwidget.viewer_widget3D import MoleculeViewer3DWidget
 from fastmolwidget.molecule_base import MoleculeWidgetProtocol
 from fastmolwidget.sdm import Atomtuple
 
-__version__ = "0.9.3"
+__version__ = "0.9.4"
 
 __all__ = [
     "MoleculeViewerWidget",
